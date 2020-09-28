7ee8a85d100f9cba85c44705eeb0009a02a978d3
Joy Kitson <jkitson@umd.edu>
2020-09-28 15:17:17 -0400

    cherry pick of adding seperate Location class

--- src/Locations.h
+++ src/Locations.h
@@ -23,7 +23,10 @@ class Locations : public CBase_Locations {
     Locations();
     void ReceiveVisitMessages(
       int personIdx,
+<<<<<<< HEAD
       char personState,
+=======
+>>>>>>> eb53b66... now generating and sending start and end times for visits
       int locationIdx,
       int visitStart,
       int visitEnd
