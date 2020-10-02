1a9393fd3a5359684a9c7f41310005f66d496c58
Joy Kitson <jkitson@umd.edu>
2020-10-02 10:59:25 -0400

    cherry-picked Locations cleanup

--- src/Locations.C
+++ src/Locations.C
@@ -12,49 +12,89 @@
 
 Locations::Locations() {
   // getting number of locations assigned to this chare
-  numLocalLocations = getNumLocalElements(numLocations, numLocationPartitions, thisIndex);
+  numLocalLocations = getNumLocalElements(
+    numLocations,
+    numLocationPartitions,
+    thisIndex
+  );
   visitors.resize(numLocalLocations);
+  
   // Init disease states.
   diseaseModel = globDiseaseModel.ckLocalBranch();
   locationState.resize(numLocalLocations, diseaseModel->getHealthyState());
+  
   // Seed random number generator via branch ID for reproducibility.
   generator.seed(thisIndex);
-  MAX_RANDOM_VALUE = (float)generator.max();
+  MAX_RANDOM_VALUE = (float) generator.max();
 }
 
 void Locations::ReceiveVisitMessages(int personIdx, int personState, int locationIdx) {
   // adding person to location visit list
-  int localLocIdx = getLocalIndex(locationIdx, numLocations, numLocationPartitions);
-  visitors[localLocIdx].push_back(std::pair<int,char>(personIdx, personState));
-  if(diseaseModel->isInfectious(personState))
+  int localLocIdx = getLocalIndex(
+    locationIdx,
+    numLocations,
+    numLocationPartitions
+  );
+  
+  if(personState == INFECTIOUS)
     locationState[localLocIdx] = INFECTIOUS;
-  // CkPrintf("Location %d localIdx %d visited by person %d\n", locationIdx, localLocIdx, personIdx);
+  
+  visitors[localLocIdx].push_back(
+    std::pair<int,char>(personIdx, personState)
+  );
+  //CkPrintf(
+  //  "Location %d localIdx %d visited by person %d\n",
+  //  locationIdx,
+  //  localLocIdx,
+  //  personIdx
+  //);
 }
 
 void Locations::ComputeInteractions() {
   int peopleSubsetIdx;
-  int cont=0, globalIdx;
+  int cont = 0, globalIdx;
+  char state;
   float value;
   // traverses list of locations
-  for(std::vector<std::vector<std::pair<int,char> > >::iterator locIter = visitors.begin() ; locIter != visitors.end(); ++locIter) {
-    if(locationState[cont] == INFECTIOUS) {
-      // sorting set of people to guarantee deterministic behavior
-      sort(locIter->begin(), locIter->end());
-      globalIdx = getGlobalIndex(cont, thisIndex, numLocations, numLocationPartitions);
-      for(std::vector<std::pair<int,char> >::iterator visitor = locIter->begin() ; visitor != locIter->end(); ++visitor) {
-        // randomly selecting people to get infected
-        value = (float)generator();
-        // CkPrintf("Partition %d - Location %d - Person %d - Value %f\n", thisIndex, globalIdx, *visitor, value);
-        if(visitor->second == diseaseModel->getHealthyState() && value/MAX_RANDOM_VALUE < INFECTION_PROBABILITY) {
-          peopleSubsetIdx = getPartitionIndex(visitor->first, numPeople, numPeoplePartitions);
-          peopleArray[peopleSubsetIdx].ReceiveInfections(visitor->first);
-        }
+  for (auto locIter : visitors) {
+    // sorting set of people to guarantee deterministic behavior
+    sort(locIter.begin(), locIter.end());
+    //globalIdx = getGlobalIndex(
+    //  cont,
+    //  thisIndex,
+    //  numLocations,
+    //  numLocationPartitions
+    //);
+
+    for(auto it : locIter) {
+      // randomly selecting people to get infected
+      value = (float) generator();
+      
+      //CkPrintf(
+      //  "Partition %d - Location %d - Person %d - Value %f\n",
+      //  thisIndex,
+      //  globalIdx,
+      //  *it,
+      //  value
+      //);
+      
+      if (value/MAX_RANDOM_VALUE < INFECTION_PROBABILITY) {
+        state = INFECTED;
+      } else {
+        state = HEALTHY;
       }
+      
+      peopleSubsetIdx = getPartitionIndex(
+        it,
+        numPeople,
+        numPeoplePartitions
+      );
+      peopleArray[peopleSubsetIdx].ReceiveInfections(it, state);
     }
-    // cleaning the visits to this location
-    locIter->clear();
+
     cont++;
   }
+  
   // cleaning state of all locations
   locationState.resize(numLocalLocations, diseaseModel->getHealthyState());
 }
