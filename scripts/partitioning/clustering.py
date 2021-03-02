505bb688c862fa1670ae9ae75ffb35324663b381
Ian Costello <iancostello1@gmail.com>
2021-03-02 10:38:52 -0500

    Introduces scripts to perform pre-run offline load balancing

---
+++ scripts/partitioning/clustering.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+from .. import location_partitioners as lp
+from .. import people_partitions as pp
+
+import sys
+sys.path.append("../")
+from helpers import id_remapper
+
+LOCATION_SCHEMES = ["VISIT", "GEO"]
+PEOPLE_SCHEMES = ["GREEDY"]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Scripts to perform pre-run load balancing.")
+    parser.add_argument("location_method", help=f"Location partitioning scheme to use: {LOCATION_SCHEMES}")
+    parser.add_argument("people_method", help=f"People partitioning scheme to use: {PEOPLE_SCHEMES}")
+    parser.add_argument("num_people_partitions")
+    parser.add_argument("num_location_partitions")
+    parser.add_argument("data_path", help=f"Path to root datafile.")
+    parser.add_argument("output_path", help=f"Where to write rewritten files.")
+    args = parser.parse_args()
+    assert(args.location_method in LOCATION_SCHEMES)
+    assert(args.people_method in PEOPLE_SCHEMES)
+    
+    # Read datafiles.
+    people = pd.read_csv(f"{parser.data_path}/people.csv")
+    locations = pd.read_csv(f"{parser.data_path}/locations.csv")
+    visits = pd.read_csv(f"{parser.data_path}/visits.csv")
+
+    # Build PeopleXLocation visit graph. Rows represent locations, columns are people
+    # and the values are the number of times that person visits the location.
+    people_location_visit_graph = np.zeros((locations.shape[0], people.shape[0]))
+    for _, row in visits[['pid','lid','start_time']].groupby(['lid','pid']).count().reset_index().iterrows():
+        people_location_visit_graph[row['lid']][row['pid']] = row['start_time']
+
+    # Create location clusters.
+    location_clusters = None
+    if args.location_method == "VISIT":
+        location_clusters = lp.graph_partioning_clustering(locations, args.num_location_partitions, people_location_visit_graph)
+    else:
+        location_clusters = lp.geo_clustering(locations, 4,  args.num_location_partitions)
+
+    # Create people clusters.
+    people_clusters = pp.greedy_affinity_to_clusters(location_clusters, args.num_people_partitions, people_location_visit_graph)
+
+    # Remap all ids to new scheme.  
+    people_remapped_ids = []
+    for p_cluster in people_clusters:
+        people_remapped_ids.extend(p_cluster)
+    location_remapped_ids = []
+    for l_cluster in location_clusters:
+        location_remapped_ids.extend(l_cluster)
+    people, locations, visits = id_remapper.remap(people.reindex(people_remapped_ids), locations.reindex(people_remapped_ids), visits, people_remapped_ids, location_remapped_ids)    
+
+    # Output
+    people.to_csv(f"{args.output_path}/people.csv", index=False)
+    locations.to_csv(f"{args.output_path}/locations.csv", index=False)
+    visits.to_csv(f"{args.output_path}/visits.csv", index=False)
+
+
---
+++ scripts/partitioning/clustering_helpers.py
@@ -0,0 +1,55 @@
+def split_large_clusters(clusters, max_in_cluster):
+    """ Breaks clusters that are above the maximum size into (n-1) full clusters
+    and 1 partially fully cluster. """
+    # Detects if any clusters are oversized and if so creates a list of new
+    # clusters to replace that one.
+    new_clusters = []
+    for name, values in clusters.items():
+        total_in_cluster = len(values)
+        if total_in_cluster > max_in_cluster:
+            temp = []
+            for new_cluster_num, i in enumerate(range(0, total_in_cluster, max_in_cluster)):
+                temp.append((f"{name}_{new_cluster_num}", values[i:i+max_in_cluster]))
+            new_clusters.append((name, temp))
+
+    # Replace single large clusters with batch of smaller ones
+    for old_cluster_name, new_batch in new_clusters:
+        del clusters[old_cluster_name]
+        for name, values in new_batch:
+            clusters[name] = values
+    return clusters
+
+def recombine_clusters(clusters, max_in_cluster):
+    """ Given a list of complete and incomplete clusters, this function combines
+    those clusters into n-1 full clusters and at most 1 partial cluster. """
+    if len(clusters) <= 1:
+        return clusters
+        
+    # We will shift data from the clusters at the back to the front
+    keys = list(clusters.keys())
+    c_front = 0
+    c_back = len(keys) - 1
+    while c_front != c_back:
+        # Calculate how many elements the front cluster needs to be complete.
+        lacking_from_front = max(0, max_in_cluster - len(clusters[keys[c_front]]))
+        if lacking_from_front == 0:
+            c_front += 1
+            continue
+            
+        # May or may not be enough elements in the current last cluster to give.
+        back_to_give = len(clusters[keys[c_back]])
+        if lacking_from_front >= back_to_give:
+            # Can shift all the keys from the back.
+            clusters[keys[c_front]].extend(clusters[keys[c_back]])
+            del clusters[keys[c_back]]
+            c_back -= 1
+            if lacking_from_front == back_to_give:
+                if c_front == c_back:
+                    return clusters
+                c_front += 1
+        else:
+            # Back has too many to give so pop these off.
+            for _ in range(lacking_from_front):
+                clusters[keys[c_front]].append(clusters[keys[c_back]].pop(0))
+            c_front += 1
+    return clusters
