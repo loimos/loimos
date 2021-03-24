626fa4bd296344c4d14474644181fa1a4c91e7aa
Ian Costello <iancostello1@gmail.com>
2021-03-23 21:50:07 -0400

    Fixed ID remapper

--- scripts/utils/id_remapper.py
+++ scripts/utils/id_remapper.py
@@ -1,28 +1,22 @@
 def remap(people, locations, visits, new_people_ids, new_location_ids):
-    groups = [
-        (people, 'pid', new_people_ids),
-        (locations, 'lid', new_location_ids)
-    ]
+    # Remap person ids.
+    people = people.reset_index(drop=True).copy()
+    people['temp_id'] = people.index
+    person_remapper = people[['pid', 'temp_id']].copy()
+    people['pid'] = people['temp_id']
+    people.drop(["temp_id"], axis=1, inplace=True)
+    visits = visits.merge(person_remapper, left_on='pid', right_on='pid')
+    visits['pid']= visits['temp_id']
+    visits.drop(["temp_id"], axis = 1, inplace=True)
 
     # Remap location ids.
+    locations = locations.reset_index(drop=True).copy()
+    locations['temp_id'] = locations.index
+    loc_remapper = locations[['lid', 'temp_id']].copy()
+    locations['lid'] = locations['temp_id']
+    locations.drop(["temp_id"], axis=1, inplace=True)
+    visits = visits.merge(loc_remapper, left_on='lid', right_on='lid')
+    visits['lid']= visits['temp_id']
+    visits.drop(["temp_id"], axis = 1, inplace=True)
     
-    new_data = []
-    for to_remap, key, new_ids in groups:
-        # Remaps the dataframes existing index to a new dense index.
-        to_remap = to_remap.reset_index(drop=True).copy()
-        to_remap['new_id'] = to_remap.index
-        remapper = to_remap[[key, 'new_id']].copy()
-        to_remap[key] = to_remap['new_id']
-        to_remap.drop(["new_id"], axis=1, inplace=True)
-        new_data.append(to_remap)
-
-        # Replaced foreign key references.
-        # for df in foreign_dfs:
-        # Replace all references of the old keys with the new ones
-        visits = visits.merge(remapper, left_on=key, right_on=key)
-        visits[key]= visits['new_id']
-        visits.drop(["new_id"], axis = 1, inplace=True)
-            
-    new_data.append(visits)
-    return new_data
     return people, locations, visits
