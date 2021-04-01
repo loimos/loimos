71aa386250cadba0908b14052ce45cd158e9a63b
Joy Kitson <jkitson@umd.edu>
2021-04-01 15:52:16 -0400

    working changes

--- src/Defs.h
+++ src/Defs.h
@@ -29,8 +29,8 @@ const Time HOUR_LENGTH = 3600;
 const Time MINUTE_LENGTH = 60;
 
 // CSV Order
-#define AGE_CSV_LOC 0
-#define LOCATIONS_MAX_VISITS_INDEX 1
+#define AGE_CSV_INDEX 0
+#define SIMULTANEOUS_MAX_VISITS_CSV_INDEX 1
 
 // Data loading
 #define EMPTY_VISIT_SCHEDULE 0xFFFFFFFF
