8766a18d3e56fdc3746692bca5cc12eef22a2f1f
Joy Kitson <jkitson@umd.edu>
2021-09-10 14:55:16 -0400

    uncommented line in run-pipeline to copy data form original location to data directory

--- scripts/batch/run-pipeline.sh
+++ scripts/batch/run-pipeline.sh
@@ -67,7 +67,7 @@ run_script combine-household-data.sh ${BASE_TIME} standard 1 ${BASE_MEMORY}
 # From here on we no longer need any of all the raw data, so move all the
 # processed data (which will be at the top level of the input dir) over to
 # the otput dir
-#mv ${IN_DIR}/*.csv ${OUT_DIR}
+mv ${IN_DIR}/*.csv ${OUT_DIR}
 
 run_script merge-location-data.py ${BASE_TIME} standard 1 ${BASE_MEMORY}
 run_script location-heuristics.py $((2 * ${BASE_TIME})) parallel 40 ${BASE_MEMORY}
