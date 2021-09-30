9325df073510312d78620353e4b6f2f9e07c3914
Diego <djimenez@thetalogin6.tmi.alcf.anl.gov>
2021-09-30 13:26:52 +0000

    Uploading merged files after rebasing branch with develop. Added a PROJECTIONS_FLAG to the Makefile so that this mode can be enabled/disabled when needed

--- src/Main.C
+++ src/Main.C
@@ -25,6 +25,7 @@
 /* readonly */ CProxy_People peopleArray;
 /* readonly */ CProxy_Locations locationsArray;
 /* readonly */ CProxy_DiseaseModel globDiseaseModel;
+/* readonly */ CProxy_TraceSwitcher traceArray;
 /* readonly */ int numPeople;
 /* readonly */ int numLocations;
 /* readonly */ int numPeoplePartitions;
@@ -50,6 +51,21 @@
 /* readonly */ int synLocationPartitionGridWidth;
 /* readonly */ int synLocationPartitionGridHeight;
 /* readonly */ int averageDegreeOfVisit;
+
+
+class TraceSwitcher : public CBase_TraceSwitcher {
+public:
+    TraceSwitcher() : CBase_TraceSwitcher(){}
+    void traceOn(){
+        traceBegin();
+        contribute(CkCallback(CkReductionTarget(Main, traceSwitchOn),mainProxy));
+    };
+    void traceOff(){
+        traceEnd();    
+        contribute(CkCallback(CkReductionTarget(Main, traceSwitchOff),mainProxy));
+    };   
+    
+};
 
 Main::Main(CkArgMsg* msg) {
   // parsing command line arguments
@@ -163,6 +179,7 @@ Main::Main(CkArgMsg* msg) {
   
   peopleArray = CProxy_People::ckNew(numPeoplePartitions);
   locationsArray = CProxy_Locations::ckNew(numLocationPartitions);
+  traceArray = CProxy_TraceSwitcher::ckNew();
 
   // run
   CkPrintf("Running ...\n\n");
@@ -204,5 +221,7 @@ void Main::SaveStats(int *data) {
 
   outFile.close();
 }
+
+
 
 #include "loimos.def.h"
