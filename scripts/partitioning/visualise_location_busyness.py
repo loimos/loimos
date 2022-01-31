e8587c54f238afbd8b90b95d896310b6d3498e63
Joy Kitson <jkitson@umd.edu>
2022-01-31 10:26:53 -0500

    added script for plotting the sum of max_simultaneous visits per partition when an input file is split into n [roughly] equal partitions in the manner done by the Loimos runtime

---
+++ scripts/partitioning/visualise_location_busyness.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
+import sys
+import math
+
+#input_dir = os.path.join('..', '..', 'data', 'populations', 'coc')
+#n_partitions = 40
+input_dir = sys.argv[1]
+n_partitions = int(sys.argv[2])
+
+locations_path = os.path.join(input_dir, 'locations.csv')
+locations = pd.read_csv(locations_path)
+
+# Plot distribution
+sns.jointplot(data=locations, x='lid', y='max_simultaneous_visits', kind='kde')
+
+# Partition data
+partition_width = int(math.ceil(locations.shape[0] / n_partitions))
+locations['lid_partition'] = pd.cut(locations['lid'], n_partitions).cat.codes
+partitions = pd.DataFrame(locations.groupby(by='lid_partition').sum())
+
+# Show how heavy each partition is
+fig, ax = plt.subplots(figsize=(10,6))
+sns.barplot(data=locations, x='lid_partition', y='max_simultaneous_visits', ax=ax)
+plt.savefig('partition_weights.png')
+
+# Show the distribution in each partition (also shows outliers)
+fig, ax = plt.subplots(figsize=(10,6))
+sns.boxplot(data=locations, x='lid_partition', y='max_simultaneous_visits', ax=ax)
+plt.savefig('partition_distribution.png')
+
+#plt.fill_between(partitions.index, partitions['max_simultaneous_visits'])
