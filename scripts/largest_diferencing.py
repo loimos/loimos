7c41bd13fce23fd8ca5605767d8a53f458c1f639
Joy Kitson <jkitson@umd.edu>
2022-02-02 11:44:56 -0500

    fixed indexing so that we can properly permute the location data (needed to pair new and old indices and sort by new indices in order to be able to match the format expected by remap)

--- scripts/largest_diferencing.py
+++ scripts/largest_diferencing.py
@@ -3,7 +3,9 @@
 import sys
 import os
 import itertools
+import numpy as np
 import pandas as pd
+import pprint
 
 from numberpartitioning import karmarkar_karp
 from utils.id_remapper import remap
@@ -18,16 +20,33 @@ def main():
     visits = pd.read_csv(os.path.join(path, 'visits.csv'))
     num_parts = int(sys.argv[2])
 
+    # Partition data and combine new indices into a single list for easier
+    # indexing
     results = karmarkar_karp(list(locations['max_simultaneous_visits']),
             num_parts=num_parts, return_indices=True)
-    permutation = list(itertools.chain(*results.partition))
+    print('parition lengths:', [len(p) for p in results.partition])
+    permutation = np.fromiter(itertools.chain(*results.partition), int)
 
-    remap(people, locations, visits, people['pid'],
-            locations.iloc[permutation]['lid'])
+    # Remap expects an array of new indices in the same order as the orignal
+    # dataframe, so we need to convert from the order the indices appear in
+    # the output to the order they appear in the input
+    cauchy_perm = np.array([locations.iloc[permutation]['lid'], permutation])
+    cauchy_perm = cauchy_perm.transpose()
+    cauchy_perm = cauchy_perm[cauchy_perm[:, 0].argsort()]
+    print('(lid, new lid) pairs:')
+    print(cauchy_perm)
+
+    #print('indices:', locations.index)
+    #print('permutation:', permutation)
+    #print('permuted indices:', locations.index[permutation])
+    people, locations, visits = remap(people, locations, visits, people['pid'],
+            cauchy_perm[:,1])
+    # Loimos expects partitions to be in order, so we need to sort the data
+    # to reflect the new indicies before writing everythign out
     locations.sort_values(by='lid', inplace=True)
 
-    print(locations.columns)
-    print(locations)
+    #print(locations.columns)
+    #print(locations)
 
     people.to_csv('people.csv', index=False)
     locations.to_csv('locations.csv', index=False)
