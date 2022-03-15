37ddcc236e71fe2076a72ec8298779cd9b66cf82
Joy Kitson <jkitson@umd.edu>
2022-03-14 23:18:58 -0400

    changed mere-locaition-data.py to make sure it doesn't accidentally delete people (and updates the hid column instead of the non-existent lid column in the people data)

--- scripts/batch/run-visualise-location-business.sh
+++ scripts/batch/run-visualise-location-business.sh
@@ -5,8 +5,14 @@
 #SBATCH -N 1
 #SBATCH --account=biocomplexity
 
+RAW_DIR=../../data/populations/wy_old
+PARTITIONED_DIR=./
+
 module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 mvapich2/2.3.3 \
   openmpi/3.1.6 python/3.8.8
 
-../partitioning/visualise_location_busyness.py  ../../data/populations/wy_old 576
-#../partitioning/visualise_location_busyness.py  ./ 576
+../partitioning/visualise_location_busyness.py  ${RAW_DIR} 2304
+mv partition_weights.pdf before_lb_partition_weights.pdf
+
+../partitioning/visualise_location_busyness.py  ${PARTITIONED_DIR} 2304
+mv partition_weights.pdf after_lb_partition_weights.pdf
