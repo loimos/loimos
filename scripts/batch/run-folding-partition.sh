b02be290d99ad5bbf2bb8a723d0496bbd09737bd
Joy Kitson <jkitson@umd.edu>
2022-03-16 09:46:18 -0400

    can now specify a state when submitting run-folding-partition.sh as a batch script

--- scripts/batch/run-folding-partition.sh
+++ scripts/batch/run-folding-partition.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #SBATCH -q normal
-#SBATCH -p standard
-#SBATCH -t 30
+#SBATCH -p largemem
+#SBATCH -t 120
 #SBATCH -N 1
 #SBATCH --account=biocomplexity
+
+STATE=${1}
+NUM_PARTITIONS=${2}
+if [ -z ${NUM_PARTITIONS} ]; then
+  NUM_PARTITIONS=576
+fi
 
 module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 mvapich2/2.3.3 \
   openmpi/3.1.6 python/3.8.8
 
-../partitioning/folding_partition.py  ../../data/populations/wy-v0 2304
+echo ../partitioning/folding_partition.py  ../../data/populations/${STATE} ${NUM_PARTITIONS}
+time ../partitioning/folding_partition.py  ../../data/populations/${STATE} ${NUM_PARTITIONS}
+
+mv locations.csv ${STATE}_locations.csv
