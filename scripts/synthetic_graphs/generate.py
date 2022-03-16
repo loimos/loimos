49968d89bd3d097fb221bb83d3578d37162c4570
Diego <djimenez@thetalogin5.tmi.alcf.anl.gov>
2022-03-16 17:42:36 +0000

    Modified the synthetic_graph generation scripts so that a number of desired people can be specified. This number is used with a Poisson distribution to get an approximate amount of people close to the desired amount

--- scripts/synthetic_graphs/generate.py
+++ scripts/synthetic_graphs/generate.py
@@ -13,10 +13,11 @@ watts_strogatz is parameterized by (num_nodes, mean_degree_k, beta)
 See random_graphs for information about these parameters.
 
 Example Usage 1
-python3 generate.py --strategy=barabasi_albert --parameters=100,5
-Generates a Barabasi Albert random graph with 100 nodes and mean degree 5
-and then transcribes this to a 100 location population graph with approximately
-400 people each with about 5 visits per day.
+python3 generate.py --strategy=barabasi_albert --parameters=100,5,233
+Generates a Barabasi Albert random graph with 100 nodes and mean degree 5, and uses the 
+amount of people (233) to generated a poisson distribution of people per location centered 
+around lambda = num_people/num_locations. This would transcribe to a 100 location graph,
+with a number of people roughly around the given num_people parameter each with about 5 visits per day.
 
 Example Usage 2
 python3 generate.py --strategy=watts_strogatz --parameters=100,5,0.3
@@ -61,21 +62,23 @@ def main(unused_argv):
         parameters="_".join(parameters))
 
     if TRANSLATION_STRATEGY.value == "barabasi_albert":
-        if len(parameters) != 2:
+        if len(parameters) != 3:
             raise ValueError("Incorrect number of parameters provided.")
-        num_nodes, mean_degree = parameters
+        num_nodes, mean_degree, num_people = parameters
+        mean_people = int(num_people)/int(num_nodes)
         graph = random_graphs.generate_barabasi_albert(
             int(num_nodes), int(mean_degree))
         translation_strategies.graph_to_disease_model(graph, out_dir,
-                                                      TEMPLATE_DIR.value)
+                                                      TEMPLATE_DIR.value,int(num_nodes),mean_people)
     elif TRANSLATION_STRATEGY.value == "watts_strogatz":
-        if len(parameters) != 3:
+        if len(parameters) != 4:
                 raise ValueError("Incorrect number of parameters provided.")
-        num_nodes, mean_degree_k, beta  = parameters
+        num_nodes, mean_degree_k, beta,num_people  = parameters
+        mean_people = int(num_people)/int(num_nodes)
         graph = random_graphs.generate_watts_strogatz(
             int(num_nodes), int(mean_degree_k), float(beta))
         translation_strategies.graph_to_disease_model(graph, out_dir,
-                                                      TEMPLATE_DIR.value)
+                                                      TEMPLATE_DIR.value,int(num_nodes),mean_people)
     else:
         raise ValueError("Unknown translation strategy: %s",
                          TRANSLATION_STRATEGY.value)
