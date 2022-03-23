ecc90e016b325875800775d52d63740e91f5eb0f
Joy Kitson <jkitson@umd.edu>
2022-03-23 11:53:18 -0400

    updated scripts for running the folding partition and visuizing its effects to reflect new arguments and also to properly set up the output dirs (including by linking to any neccessary files from the old directory)

--- scripts/batch/run-folding-partition.sh
+++ scripts/batch/run-folding-partition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -q normal
 #SBATCH -p largemem
-#SBATCH -t 120
+#SBATCH -t 20
 #SBATCH -N 1
 #SBATCH --account=biocomplexity
 
@@ -10,11 +10,32 @@ NUM_PARTITIONS=${2}
 if [ -z ${NUM_PARTITIONS} ]; then
   NUM_PARTITIONS=576
 fi
+ARGS=${3}
+if [ -z ${ARGS} ]; then
+  ARGS='-p "average_daily_total"'
+fi
+
+RAW_DIR=../../data/populations/${STATE}
+PARTITIONED_DIR=../../data/populations/${STATE}_partitioned
+
+if [ ! -d ${PARTITIONED_DIR} ]; then
+ mkdir ${PARTITIONED_DIR}
+ echo "Made dir ${PARTITIONED_DIR}"
+ echo
+fi
+
+# All of the files in the original dir are important, so link to any that
+# are missing
+for file in ${RAW_DIR}/*; do
+ filename=$(basename ${file})
+ if [[ ${filename} != *".csv"* && ! -e ${PARTITIONED_DIR}/${filename} ]]; then
+   ln -s ${RAW_DIR}/${filename} ${PARTITIONED_DIR}/${filename}
+   echo "  Created link from ${RAW_DIR}/${filename} to ${PARTITIONED_DIR}/${filename}"
+ fi
+done
 
 module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 mvapich2/2.3.3 \
   openmpi/3.1.6 python/3.8.8
 
-echo ../partitioning/folding_partition.py  ../../data/populations/${STATE} ${NUM_PARTITIONS}
-time ../partitioning/folding_partition.py  ../../data/populations/${STATE} ${NUM_PARTITIONS}
-
-mv locations.csv ${STATE}_locations.csv
+echo ../partitioning/folding_partition.py  ${RAW_DIR} -o ${PARTITIONED_DIR} -n ${NUM_PARTITIONS} ${ARGS}
+time ../partitioning/folding_partition.py  ${RAW_DIR} -o ${PARTITIONED_DIR} -n ${NUM_PARTITIONS} ${ARGS}
--- scripts/batch/run-visualise-location-business.sh
+++ scripts/batch/run-visualise-location-business.sh
@@ -5,14 +5,24 @@
 #SBATCH -N 1
 #SBATCH --account=biocomplexity
 
-RAW_DIR=../../data/populations/wy_old
-PARTITIONED_DIR=./
+STATE=${1}
+NUM_PARTITIONS=${2}
+if [ -z ${NUM_PARTITIONS} ]; then
+  NUM_PARTITIONS=576
+fi
+ARGS=${3}
+if [ -z ${ARGS} ]; then
+  ARGS='-p "average_daily_total"'
+fi
+
+RAW_DIR=../../data/populations/${STATE}
+PARTITIONED_DIR=../../data/populations/${STATE}_partitioned
 
 module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 mvapich2/2.3.3 \
   openmpi/3.1.6 python/3.8.8
 
-../partitioning/visualise_location_busyness.py  ${RAW_DIR} 2304
-mv partition_weights.pdf before_lb_partition_weights.pdf
+../partitioning/visualise_location_busyness.py  ${RAW_DIR} \
+  -n ${NUM_PARTITIONS} -o ${RAW_DIR} ${ARGS}
 
-../partitioning/visualise_location_busyness.py  ${PARTITIONED_DIR} 2304
-mv partition_weights.pdf after_lb_partition_weights.pdf
+../partitioning/visualise_location_busyness.py  ${PARTITIONED_DIR} \
+  -n ${NUM_PARTITIONS} -o ${PARTITIONED_DIR} ${ARGS}
