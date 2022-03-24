3a4af9b2221666245883cc4dab47f0a334e1e3b7
Joy Kitson <jkitson@umd.edu>
2022-03-24 16:58:42 -0400

    updated validation script to partition befroe merging, in order to avoid memory issues

--- scripts/validate.py
+++ scripts/validate.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 import argparse
 
+from utils.ids import partition_df
+
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -16,6 +18,17 @@ def parse_args():
             help='The path to the directory containing the input files')
 
     return parser.parse_args()
+
+def partitioned_merge(left, right, on, num_partitions=128, args={}):
+    left_partitioned = partition_df(left, on=on, num_partitions=num_partitions)
+    right_partitioned = partition_df(right, on=on, num_partitions=num_partitions)
+
+    merged_partitioned = [left_partitioned[i].merge(
+        right_partitioned[i], on=on, **args
+    ) for i in range(num_partitions-1)]
+    merged_df = pd.concat(merged_partitioned)
+
+    return merged_df
 
 def main():
     # Parse args and read data
@@ -26,16 +39,23 @@ def main():
     locations = pd.read_csv(os.path.join(input_dir, 'locations.csv'))
     visits = pd.read_csv(os.path.join(input_dir, 'visits.csv'))
 
-    # Make sure ids are contiguous
+    print('Make sure ids are contiguous')
     assert(np.all(people['pid'] == people.index))
     assert(np.all(locations['lid'] == locations.index))
 
-    # Make sure all locations and people referenced in visits exist
-    visitors = people.merge(visits, on='pid', how='inner',
-            validate='one_to_many')
+    print('Make sure all locations and people referenced in visits exist')
+    visitors = partitioned_merge(people, visits, 'pid',
+            args={'how': 'inner', 'validate': 'one_to_many'})
+    visited = partitioned_merge(locations, visits, 'lid',
+            args={'how': 'inner', 'validate': 'one_to_many'})
+    #people.merge(visits, on='pid', how='inner',
+    #        validate='one_to_many')
+    #visited = locations.merge(visits, on='lid', how='inner',
+    #        validate='one_to_many')
+    print(f'  total visits: {visits.shape[0]}')
+    print(f'  visits with valid pid: {visitors.shape[0]}')
+    print(f'  visits with valid lid: {visited.shape[0]}')
     assert(visitors.shape[0] == visits.shape[0])
-    visited = locations.merge(visits, on='lid', how='inner',
-            validate='one_to_many')
     assert(visited.shape[0] == visits.shape[0])
 
     print('All tests passed!')
