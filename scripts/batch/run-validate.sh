ca3fabc3abcd573fba7d7357ec1a7b70af6e558f
Joy Kitson <jkitson@umd.edu>
2022-03-24 15:46:27 -0400

    added script for validating scenario files

---
+++ scripts/batch/run-validate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -q normal
+#SBATCH -p largemem
+#SBATCH -t 10
+#SBATCH -N 1
+#SBATCH --account=biocomplexity
+
+STATE=${1}
+
+INPUT_DIR=../../data/populations/${STATE}
+
+module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 mvapich2/2.3.3 \
+  openmpi/3.1.6 python/3.8.8
+
+for dir in ${INPUT_DIR}* ; do
+  echo Testing ${dir}
+  time ../validate.py  ${dir}
+done
