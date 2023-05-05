f45e625ea4b0cb3f8e134658d2a52c665e0d7512
Joy Kitson <jkitson@umd.edu>
2023-05-05 16:18:44 -0400

    fixed issue where we would seed the outbreak using non-exisitent people when the first pid is not 0. Now add first pid to both min and max pid in the seeding pid distribution.

--- src/Main.cpp
+++ src/Main.cpp
@@ -400,7 +400,8 @@ void Main::SeedInfections() {
   // guarentee they are unique (not checking this quickly runs into birthday
   // problem issues, even for sizable datasets)
   if (0 == day) {
-    std::uniform_int_distribution<> personDistrib(0, numPeople - 1);
+    std::uniform_int_distribution<> personDistrib(firstPersonIdx,
+        firstPersonIdx + numPeople - 1);
     std::unordered_set<int> initialInfectionsSet;
     initialInfections.reserve(INITIAL_INFECTIONS);
     // Use set to check membership becuase it's faster and we can spare the
