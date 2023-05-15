8ed7a6179b9f5fc91fe684958bb96113b568b1a0
Joy Kitson <jkitson@umd.edu>
2023-05-15 15:07:16 -0400

    added code to check whether or not two events overlap

--- src/Event.cpp
+++ src/Event.cpp
@@ -30,7 +30,7 @@ bool Event::operator<(const Event& rhs) const {
 
 // Compares two events based on the correspodning other event (if one is an
 // arrival, the matching departure, and vice versa)
-bool Event::greaterPartner(const Event& e0, const Event& e1) {
+bool Event::greaterPartner(const Event &e0, const Event &e1) {
   // First compare times...
   if (e0.partnerTime != e1.partnerTime) {
     return e0.partnerTime > e1.partnerTime;
@@ -49,6 +49,27 @@ bool Event::greaterPartner(const Event& e0, const Event& e1) {
 
   // ...and finally breka ties with the vistior's state
   return e0.personState > e1.personState;
+}
+
+bool Event::overlap(const Event &e0, const Event &e1) {
+  int start0, end0, start1, end1;
+  if (ARRIVAL == e0.type) {
+    start0 = e0.scheduledTime;
+    end0 = e0.partnerTime;
+  } else {
+    end0 = e0.scheduledTime;
+    start0 = e0.partnerTime;
+  }
+  if (ARRIVAL == e1.type) {
+    start1 = e1.scheduledTime;
+    end1 = e1.partnerTime;
+  } else {
+    end1 = e1.scheduledTime;
+    start1 = e1.partnerTime;
+  }
+
+  return (start0 <= start1 && end0 > start1)
+    || (start0 > start1 && end1 > start0);
 }
 
 void Event::pair(Event *e0, Event *e1) {
