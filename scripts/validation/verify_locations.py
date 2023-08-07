58c8389f8520343d546a910cd22823cd4459eefc
Joy Kitson <jkitson@umd.edu>
2023-08-07 10:57:15 -0400

    Added check for error in caclculating the number of expected interactions caused by locations only present in either the Limos output or the baseline data

--- scripts/validation/verify_locations.py
+++ scripts/validation/verify_locations.py
@@ -49,22 +49,33 @@ COMPARISION_COLUMNS = ["max_occupancy", "conn_prob", "max_possible_edges",
     "num_expected_edges"]
 BASELINE_SUFFIX = "_baseline"
 def compare_column(metrics, baseline, col, merge_on="old_lid", epsilon=1e-6):
-    df = pd.merge(metrics, baseline,
-            on=merge_on, suffixes=["", BASELINE_SUFFIX])
+    df = pd.merge(metrics, baseline, validate="one_to_one", indicator=True,
+            on=merge_on, suffixes=["", BASELINE_SUFFIX], how="outer")
 
-    mask = df[col] == df[col + BASELINE_SUFFIX]
+    compare_shared_locations(df["both" == df["_merge"]], left_col=col,
+                             right_col=col + BASELINE_SUFFIX,
+                             epsilon=epsilon)
+    if not (df["_merge"] == "both").all():
+        compare_distict_locations(df, col, "computed", merge_side="left",
+                epsilon=epsilon)
+        compare_distict_locations(df, col + BASELINE_SUFFIX, "baseline",
+                merge_side="right", epsilon=epsilon)
+
+def compare_shared_locations(df, left_col, right_col, epsilon):
+    mask = df[left_col] == df[right_col]
     err = None
     std_err = None
     if not mask.all():
-        err = (df[col] - df[col + BASELINE_SUFFIX]).abs()
+        err = (df[left_col] - df[right_col]).abs()
         mask = err < epsilon
 
     matches = mask.sum()
     total = df.shape[0]
     if mask.all():
-        print(f"all {col} values match")
+        print(f"all {left_col} values match for shared locs")
     else:
-        print(f"{matches}/{total} ({matches/total:.2%}) of {col} values match")
+        print(f"{matches}/{total} ({matches/total:.2%}) of {left_col} values match "
+            + "at shared locs")
 
         out = "  "
         std_err = (err ** 2).mean()
@@ -79,15 +90,34 @@ def compare_column(metrics, baseline, col, merge_on="old_lid", epsilon=1e-6):
         else:
             out += f"total err: {total_err}, "
 
-        rel_err = total_err / df[col + BASELINE_SUFFIX].sum()
+        rel_err = total_err / df[right_col].sum()
         if isinstance(rel_err, float):
             out += f"rel err: {rel_err:0.3}"
         else:
             out += f"rel err: {rel_err}"
         print(out)
 
-        if col == "max_possible_edges":
-            df[~mask].to_csv("diff_locs.csv", index=False)
+
+def compare_distict_locations(df, col, data_source, merge_side="left",
+        epsilon=1e-6):
+    total = df.shape[0]
+    mask = f"{merge_side}_only" == df["_merge"]
+    mask_sum = mask.sum()
+    total_err = df[mask][col].sum()
+    # std_err = total_err ** 2
+
+    if 0 < mask_sum and epsilon < abs(total_err):
+        out = "  "
+        if isinstance(total_err, float):
+            out += f"  total err: {total_err:0.3}, "
+        else:
+            out += f"  total err: {total_err}, "
+
+        out += f"mean err: {df[mask][col].mean()}, "
+        out += f"from {mask_sum} {data_source} locations"
+        print(out)
+    elif 0 < mask_sum:
+        print(f"  No error contributed by {mask_sum} locs present only in {data_source}")
 
 
 def main():
