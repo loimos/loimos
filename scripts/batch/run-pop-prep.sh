99ac5bb1e2c3da02e7a3c9cc84862a58fa6bbe54
Joy Kitson <jkitson@umd.edu>
2023-08-09 13:40:55 -0400

    Removed old preprocessing scripts and relocated diease model conversion script to the preprocessign directory

--- scripts/batch/run-pop-prep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -q normal
-#SBATCH -N 1
-#SBATCH --account=biocomplexity
-
-echo ${SCRIPTS_DIR}/pop-prep.sh -s ${STATE} -i ${IN_DIR} -o ${OUT_DIR} -n
-echo -------------------------------------------------------------------------
-time ${SCRIPTS_DIR}/pop-prep.sh -s ${STATE} -i ${IN_DIR} -o ${OUT_DIR} -n
