465c0ee1282e418940cc168bd8c9c6fcc4a07492
Joy Kitson <jkitson@umd.edu>
2023-08-09 14:41:48 -0400

    Renamed some Python scripts to use underscores rather than dashes in thier filenames

--- scripts/batch/run-location-heuristics.sh
+++ scripts/batch/run-location-heuristics.sh
@@ -7,12 +7,12 @@
 #SBATCH --account=nssac_students
 
 module load gcc/9.2.0 cuda/11.0.228 openmpi/3.1.6 \
-  openmpi/3.1.6 python/3.8.8 #mvapich2/2.3.3 
+  openmpi/3.1.6 python/3.8.8 #mvapich2/2.3.3
 
 SCRIPTS_DIR="${HOME}/biocomplexity/loimos/loimos/scripts/preprocessing"
 OUT_DIR="${HOME}/biocomplexity/loimos/loimos/data/populations/coc_2.0"
 TASKS_PER_NODE=16
 
-echo ${SCRIPTS_DIR}/location-heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
+echo ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
 echo -------------------------------------------------------------------------
-time ${SCRIPTS_DIR}/location-heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
+time ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
