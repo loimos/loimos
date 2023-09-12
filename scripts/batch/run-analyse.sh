23f28d233c80568c9a735a459bdde1a52a25cc9b
joy-kitson <jkitson@umd.edu>
2023-09-12 15:06:52 -0400

    Updated visit analysis scripts to use linear x-scale (#88)
    * Updated visit analysis scripts to use linear x-scale
    
    * Fixed linter errors
    
    * commented out unused code in analyse_visit_distribution

--- scripts/batch/run-analyse.sh
+++ scripts/batch/run-analyse.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#SBATCH -q normal
 #SBATCH -p bii
-#SBATCH -t 30
+#SBATCH -t 5
 #SBATCH -N 1
-#SBATCH --account=nssac_students
-#SBATCH --exclusive
+#SBATCH --ntasks-per-node 1
+#SBATCH --account nssac_students
+#SBATCH --mem-per-cpu 300000
 
-DATASET=md_partitioned
+DATASET=md_2.0
 IN_DIR=../../data/populations/${DATASET}
 
 module load python
+
+mkdir -p ${DATASET}
 
 ../analysis/analyze_visit_distribution.py ${IN_DIR} ${DATASET}
