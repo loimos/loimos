ae5ead329f430384d324104c2dbf600b7845dd62
Joy Kitson <jkitson@umd.edu>
2023-11-06 15:23:40 -0500

    Updated preprocessign scripts to handle parsing visit data from multiple input files

--- scripts/batch/run-preprocess.sh
+++ scripts/batch/run-preprocess.sh
@@ -8,6 +8,10 @@
 cd /home/arr2vg/biocomplexity/loimos/loimos/scripts/preprocessing
 
 module load python
-STATE=va
+STATE=md
 
-time ./preprocess.py ${STATE} /project/bii_nssac/Loimos/dp-2.0/${STATE} -o ../../data/populations/${STATE}_2.0 -f
+#RAW_DIR=/project/bii_nssac/Loimos/dp-2.0/${STATE} 
+RAW_DIR=/home/hsm2v/population_data/dp_2_4_0/${STATE}
+CMD="time ./preprocess.py ${STATE} ${RAW_DIR} -o ../../data/populations/${STATE}_2.4 --activity-suffix .(\d+)-of-(\d+)$  -aai location_assignment/daily/adult/${STATE}_adult_activity_location_assignment_day.csv -aci location_assignment/daily/child/${STATE}_child_activity_location_assignment_day.csv"
+echo "$CMD"
+$CMD
