a9fab6a35eb3857d5c82fd27168d52eacdda8d1d
Joy Kitson <jkitson@umd.edu>
2024-01-29 12:13:25 -0500

    Added memory specification for run-location-heuristics in order to helpw ith larger datasets

--- scripts/batch/run-location-heuristics.sh
+++ scripts/batch/run-location-heuristics.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH -q normal
 #SBATCH -N 1
-#SBATCH --tasks-per-node 37
+#SBATCH --ntasks-per-node=37
+#SBATCH --mem=375000
 #SBATCH -p bii
-#SBATCH -t 60
+#SBATCH -t 600
 #SBATCH --account=nssac_students
 
 module load python
 
 SCRIPTS_DIR="${HOME}/biocomplexity/loimos/loimos/scripts/preprocessing"
-#OUT_DIR="${HOME}/biocomplexity/loimos/loimos/data/populations/coc_2.0"
-OUT_DIR="/scratch/${USER}/loimos/data/populations/md_2.4"
+OUT_DIR="/scratch/${USER}/loimos/data/populations/$1"
 TASKS_PER_NODE=37
 
-echo ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} #-O
+echo ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
 echo -------------------------------------------------------------------------
-time ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} #-O
+time ${SCRIPTS_DIR}/location_heuristics.py ${OUT_DIR} -n ${TASKS_PER_NODE} -O
