aaa9ce36d9a04b8e5a9016748aaedb277fad1985
Yash Sakharkar <64757237+ysakharkar@users.noreply.github.com>
2024-03-04 14:51:20 -0500

    Create graph_perturbation.py
    Takes in original population directory, proportion of visits to change, and output population directory. 
    
    Randomly replaces that proportion of visits' (without replacement) with locations that are randomly picked (with replacement). Then, the new population directory is created with the new visits.csv file and soft links to other population files in the old directory.

---
+++ scripts/perturbation/graph_perturbation.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import random
+import sys
+import os
+
+# Usage - graph_perturbation.py (path to current population directory) (proportion of visits to perturb) (path to new population directory)
+
+arguments = sys.argv
+
+try:
+    in_dir = arguments[1]
+except IndexError:
+    raise IndexError("Provide the path to a population directory as an argument please.")
+
+try:
+    p = float(arguments[2])
+    print(f"Proportion of graph to perturbate: {p}", flush=True)
+except IndexError:
+    raise IndexError("Provide the proportion of visits to perturb please.")
+
+try: 
+    out_dir = arguments[3]
+except IndexError:
+    raise IndexError("Provide the output population directory path please.")
+
+try:
+    df = pd.read_csv(os.path.join(in_dir, 'visits.csv'))
+    print("Loaded CSV " + os.path.join(in_dir, 'visits.csv') + "\n", flush=True)
+except:
+    raise FileNotFoundError("Please provide a valid path to a population directory as a command line argument.\n") 
+
+
+num_visits = len(df)
+n = int(p * num_visits)
+
+random_rows = df.sample(n)
+unique_lids = df['lid'].nunique()
+random_values = random.choices(range(unique_lids), k=n)
+
+random_rows['lid'] = random_values
+df.update(random_rows)
+
+print(f"Successfully perturbed {p} proportion of graph.", flush=True)
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+print(f"Exporting new visits.csv file to {os.path.join(out_dir, 'visits.csv')}.", flush=True)
+df.to_csv(os.path.join(out_dir, 'visits.csv'), index=False)
+
+print("CSV exported, now creating symbolic links for all other files in the population directory.", flush=True)
+soft_link_files = ['locations.csv', 'locations.textproto', 'people.csv', 'people.textproto', 'visits.textproto']
+
+for file in soft_link_files:
+    destination_path = os.path.join(out_dir, file)
+
+    os.symlink(os.path.join(in_dir, file), destination_path)
+
+print(f"Successfully created new population in directory {out_dir}", flush=True)
