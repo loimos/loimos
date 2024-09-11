f7e8667f2cb3fa30fc009815afbc3e0c1fe60742
Joy Kitson <jkitson@umd.edu>
2024-09-11 15:21:34 -0400

    fix: linter error

--- src/Locations.cpp
+++ src/Locations.cpp
@@ -315,7 +315,8 @@ void Locations::BinVisits() {
       if (location.acceptsVisit(visit) && diseaseModel->isInfectious(state.state)) {
         Time visitStart = visit.visitStart / N_VISIT_BINS;
         Time visitEnd = visit.visitEnd / N_VISIT_BINS;
-        double prop = diseaseModel->getInfectivity(state.state, state.transmissionModifier);
+        double prop = diseaseModel->getInfectivity(state.state,
+          state.transmissionModifier);
         for (Time t = visitStart; t <= visitEnd; ++t) {
           infectionPropensities[t] += prop;
         }
