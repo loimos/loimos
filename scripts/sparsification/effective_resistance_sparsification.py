c4b65f197cbf4c1446965877f6846195439fb139
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-01-21 18:25:35 -0500

    [WIP]: use RIch progress bars per-thread

--- scripts/sparsification/effective_resistance_sparsification.py
+++ scripts/sparsification/effective_resistance_sparsification.py
@@ -8,6 +8,7 @@ import pickle
 import argparse
 from time import perf_counter
 from threading import Thread
+from rich.progress import *
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -38,7 +39,9 @@ def parse_args():
 
     return parser.parse_args()
 
-def process_subset(df_subset, q, thread_results, thread_idx):
+
+def process_subset(progressbar, task, df_subset, q, thread_results, thread_idx):
+    progressbar.update(thread_idx, advance=1)
     if thread_results is not None and thread_idx is not None:
         start_time = perf_counter()
     edge_list = df_subset[['pid', 'lid']].to_numpy()  # should be 2 x m shape
@@ -47,9 +50,11 @@ def process_subset(df_subset, q, thread_results, thread_idx):
     network = Network(edge_list, weights)
     epsilon = 0.1
     method = 'kts'
+    progressbar.update(thread_idx, advance=1)
 
     Effective_R = network.effR(epsilon, method)
     EffR_Sparse = network.spl(q, Effective_R, seed=2020)
+    progressbar.update(thread_idx, advance=1)
 
     filtered_df_subset = df_subset[df_subset[['pid', 'lid']].apply(tuple, axis=1).isin(map(tuple, EffR_Sparse.E_list))]
 
@@ -57,6 +62,7 @@ def process_subset(df_subset, q, thread_results, thread_idx):
         end_time = perf_counter()
         thread_results[thread_idx] = filtered_df_subset
         print(f"worker thread {thread_idx} completed in {end_time - start_time :0.2f} seconds")
+    progressbar.update(thread_idx, advance=1)
     return filtered_df_subset
 
 def main():
@@ -70,40 +76,44 @@ def main():
     input = os.path.join(args.input_dir, 'visits.csv')
     df = pd.read_csv(input)
 
-    if args.separate_days:
-        print(f'Splitting data into subsets by day')
-        subsets = df.groupby('daynum')
-        filtered_dfs = []
-        threads = []
-        results = []
+    with Progress(TextColumn("[progress.description]{task.description}"),
+                  BarColumn(), TaskProgressColumn(),
+                  TimeElapsedColumn()) as progress_bar:
+        if args.separate_days:
+            print(f'Splitting data into subsets by day')
+            subsets = df.groupby('daynum')
+            filtered_dfs = []
+            threads = []
+            results = []
 
-        start = perf_counter()
-        for i, subset in subsets:
+            start = perf_counter()
+            for i, subset in subsets:
+                progress_task = progress_bar.add_task(f"[cyan]day {i}", total=4)
+                print(f'Initializing day {i+1} worker thread')
+                q = int(float(args.resultant_sample_size) * float(len(subset)))
+                threads.append(Thread(target=process_subset, args=(progress_bar, progress_task, subset, q, results, i)))
+                results.append(None)
+                threads[i].start()
+
+            for _, thread in enumerate(threads):
+                thread.join()
+
             print(f'Initializing day {i+1} worker thread')
-            q = int(float(args.resultant_sample_size) * float(len(subset)))
-            threads.append(Thread(target=process_subset, args=(subset, q, results, i)))
-            results.append(None)
-            threads[i].start()
+            for _, df_subset in enumerate(results):
+                filtered_dfs.append(df_subset)
 
-        for _, thread in enumerate(threads):
-            thread.join()
+            # TODO: cull multi-day visits
+            end = perf_counter()
+            final_filtered_df = pd.concat(filtered_dfs)
+        else:
+            q = int(float(args.resultant_sample_size) * float(len(df)))
+            final_filtered_df = process_subset(df, q, None, None)
 
-        print(f'Initializing day {i+1} worker thread')
-        for _, df_subset in enumerate(results):
-            filtered_dfs.append(df_subset)
+        if not os.path.exists(args.output_dir):
+            os.makedirs(args.output_dir)
 
-        # TODO: cull multi-day visits
-        end = perf_counter()
-        final_filtered_df = pd.concat(filtered_dfs)
-    else:
-        q = int(float(args.resultant_sample_size) * float(len(df)))
-        final_filtered_df = process_subset(df, q, None, None)
-
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-
-    final_filtered_df.to_csv(os.path.join(args.output_dir, 'visits.csv'), index=False)
-    print(f'complete: {os.path.join(args.output_dir, "visits.csv")}')
+        final_filtered_df.to_csv(os.path.join(args.output_dir, 'visits.csv'), index=False)
+        print(f'complete: {os.path.join(args.output_dir, "visits.csv")}')
 
 if __name__ == "__main__":
     main()
