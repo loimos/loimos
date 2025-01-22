c616ee40df78c965d94d81a50b00a235401f5dfb
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-01-21 19:47:07 -0500

    [WIP]: add progress task for debugging

--- scripts/sparsification/effective_resistance_sparsification.py
+++ scripts/sparsification/effective_resistance_sparsification.py
@@ -58,7 +58,7 @@ def process_subset(df_subset, q, thread_results, thread_idx, progressbar, progre
         start_time = perf_counter()
     edge_list = df_subset[['pid', 'lid']].to_numpy()  # should be 2 x m shape
     weights = df_subset['duration'].to_numpy()  # weight edge by visit duration
-    print(f"worker thread {thread_idx}: {len(weights)}")
+    progressbar.update(progresstask, advance=1)
 
     network = Network(edge_list, weights)
     epsilon = 0.1
@@ -131,7 +131,7 @@ def main():
             start = perf_counter()
             for i, subset in subsets:
                 if args.visual:
-                    progress_task = progress_bar.add_task(f"[cyan]day {i}", total=4)
+                    progress_task = progress_bar.add_task(f"[cyan]day {i}", total=5)
                 else:
                     print(f'Initializing day {i+1} worker thread')
                 q = int(float(args.resultant_sample_size) * float(len(subset)))
