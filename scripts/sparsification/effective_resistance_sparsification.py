86c2e9674f72b1cb5e60063a07bd94fecd06963b
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-01-21 19:15:47 -0500

    [WIP]: disable progress bar graphics on no --visual flag

--- scripts/sparsification/effective_resistance_sparsification.py
+++ scripts/sparsification/effective_resistance_sparsification.py
@@ -90,7 +90,8 @@ def main():
             print(f'Splitting data into subsets by day')
             subsets = df.groupby('daynum')
 
-            preprocessing_task = progress_bar.add_task(f"[green]splitting data into [bold]7 subsets", total=1.0)
+            if args.visual:
+                preprocessing_task = progress_bar.add_task(f"[green]splitting data into [bold]7 subsets", total=1.0)
             day_keys = sorted(subsets.groups.keys())
             unit_scaling = 1 / (60 * 60) # convert seconds to hours
             for daynum in day_keys[:-1]: 
@@ -101,7 +102,8 @@ def main():
 
                 day_df = subsets.get_group(daynum).copy()
                 for idx, row in day_df.iterrows():
-                    progress_bar.update(preprocessing_task, advance=(1.0 / (len(day_keys) * len(day_df))))
+                    if args.visual:
+                        progress_bar.update(preprocessing_task, advance=(1.0 / (len(day_keys) * len(day_df))))
                     end_time = row['start_time'] + row['duration']
                     # assume 24-hour boundary; adjust as needed
                     if end_time * unit_scaling > 24:
@@ -112,20 +114,23 @@ def main():
                         for future_day in day_keys[day_keys.index(daynum)+1:]:
                             if idx in subsets.get_group(future_day).index:
                                 df.drop(idx, inplace=True)
-            progress_bar.remove_task(preprocessing_task)
+            if args.visual:
+                progress_bar.remove_task(preprocessing_task)
             filtered_dfs = []
             threads = []
-            results = []
+            results = [None] * len(subsets)
 
             # python3 effective_resistance_sparsification.py -s ~/src/data/coc-unsparsified ~/src/data-spars-para/coc-sparsified-90 0.9
 
             start = perf_counter()
             for i, subset in subsets:
-                progress_task = progress_bar.add_task(f"[cyan]day {i}", total=4)
-                print(f'Initializing day {i+1} worker thread')
+                if args.visual:
+                    progress_task = progress_bar.add_task(f"[cyan]day {i}", total=4)
+                else:
+                    print(f'Initializing day {i+1} worker thread')
                 q = int(float(args.resultant_sample_size) * float(len(subset)))
-                threads.append(Thread(target=process_subset, args=(progress_bar, progress_task, subset, q, results, i)))
-                results.append(None)
+                t_args = (progress_bar, progress_task, subset, q, results, i) if args.visual else (subset, q, results, i)
+                threads.append(Thread(target=process_subset, args=t_args))
                 threads[i].start()
 
             for _, thread in enumerate(threads):
