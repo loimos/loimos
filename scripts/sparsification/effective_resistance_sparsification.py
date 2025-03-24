f53e1becb002994aa7961a419322256db2035a85
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2025-03-23 20:19:07 -0400

    remove visual argument from effective resistance sparsification script (corrected)

--- scripts/sparsification/effective_resistance_sparsification.py
+++ scripts/sparsification/effective_resistance_sparsification.py
@@ -6,11 +6,9 @@ import numpy as np
 import pandas as pd
 from EffectiveResistanceSampling.Network import Network
 import networkx as nx
-import pickle
 import argparse
 from time import perf_counter
 from multiprocessing import Pool, set_start_method
-from rich.progress import *
 from itertools import starmap
 
 # reference scripts:
@@ -73,18 +71,14 @@ def parse_args():
 
     return args
 
-<<<<<<< HEAD
-    if thread_results is not None and thread_idx is not None:
-        start_time = perf_counter()
+
+def process_subset(df_subset, q, epsilon = 0.1, method = 'kts'):
     edge_list = df_subset[['pid', 'lid']].to_numpy()  # should be 2 x m shape
     weights = df_subset['duration'].to_numpy()  # weight edge by visit duration
 
     network = Network(edge_list, weights)
-    # NOTE: ask abt undirected vs directed for e-list to adj list
 
 
-    epsilon = 0.1
-    method = 'kts'
     print("running effective resistance", flush=True)
     Effective_R = network.effR(epsilon, method)
     print("effective resistance complete", flush=True)
@@ -95,57 +89,6 @@ def parse_args():
 
     filtered_df_subset = df_subset[df_subset[['pid', 'lid']].apply(tuple, axis=1).isin(map(tuple, EffR_Sparse.E_list))]
 
-    if thread_results is not None and thread_idx is not None:
-        end_time = perf_counter()
-        thread_results[int(thread_idx)] = filtered_df_subset
-        print(f"worker thread {thread_idx} completed in {end_time - start_time :0.2f} seconds", flush=True)
-        times[int(thread_idx)] = end_time - start_time
-
-=======
-
-def process_subset(df_subset, q, thread_index=0, progressbar=None,
-                   progresstask=None, epsilon = 0.1, method = 'kts'):
-    if progressbar is not None:
-        progressbar.update(progresstask, advance=1)
-
-    edge_list = df_subset[['pid', 'lid']].to_numpy()  # should be 2 x m shape
-    weights = df_subset['duration'].to_numpy()  # weight edge by visit duration
-
-    network = None
-    if progressbar is not None:
-        subtask = progressbar.add_task(f"[cyan]interval {thread_idx}[/cyan] -- network init", total=1.0)
-        network = Network(edge_list, weights, progress_bar=progressbar, progress_task=subtask)
-        progressbar.remove_task(subtask)
-    else:
-        network = Network(edge_list, weights)
-        # NOTE: ask abt undirected vs directed for e-list to adj list
-
-
-    if progressbar is not None:
-        progressbar.update(progresstask, advance=1)
-        subtask = progressbar.add_task(f"[cyan]interval {thread_idx}[/cyan] -- effective resistance", total=1.0)
-        Effective_R = network.effR(epsilon, method, progress_bar=progressbar, progress_task=subtask)
-        progressbar.remove_task(subtask)
-    else:
-        print("running effective resistance", flush=True)
-        Effective_R = network.effR(epsilon, method)
-        print("effective resistance complete", flush=True)
-
-    if progressbar is not None:
-        subtask = progressbar.add_task(f"[cyan]interval {thread_idx}[/cyan] -- network.spl", total=1.0)
-        EffR_Sparse = network.spl(q, Effective_R, progressbar, subtask, seed=2020)
-        progressbar.remove_task(subtask)
-        progressbar.update(progresstask, advance=1)
-    else:
-        print("running network.spl", flush=True)
-        EffR_Sparse = network.spl(q, Effective_R, seed=2020)
-        print("network.spl complete", flush=True)
-
-    filtered_df_subset = df_subset[df_subset[['pid', 'lid']].apply(tuple, axis=1).isin(map(tuple, EffR_Sparse.E_list))]
-
-    if progressbar is not None:
-        progressbar.update(progresstask, advance=1)
->>>>>>> 64b7ca29a4e19310ca7fe8c92c4bf3c132dd1ede
     return filtered_df_subset
 
 def main():
@@ -171,88 +114,63 @@ def main():
     # print("EffectiveResistanceSampling repo updated")
 
 
-    with Progress(TextColumn("[progress.description]{task.description}"),
-                  BarColumn(), TaskProgressColumn(),
-                  TimeElapsedColumn()) as progress_bar:
-        if not args.visual:
-            progress_bar = None
+    if args.split is not None:
+        num_subsets = int(args.split)
 
-        if args.split is not None:
-            num_subsets = int(args.split)
+        times = {}
 
-            times = {}
+        days = df.groupby('daynum')
+        num_days = len(days.groups.keys())
+        seconds_in_day = (24 * 60.0 * 60.0)
 
-            days = df.groupby('daynum')
-            num_days = len(days.groups.keys())
-            seconds_in_day = (24 * 60.0 * 60.0)
+        subset_size = (seconds_in_day * num_days) / num_subsets
+        print("Sparsifying:", flush=True)
+        print(f"{num_subsets} intervals of length {subset_size} seconds each", flush=True)
+        def subset_num(col):
+            return np.floor(col / subset_size)
 
-            subset_size = (seconds_in_day * num_days) / num_subsets
-            print("Sparsifying:", flush=True)
-            print(f"{num_subsets} intervals of length {subset_size} seconds each", flush=True)
-            def subset_num(col):
-                return np.floor(col / subset_size)
+        subsets = df.groupby(subset_num(df['start_time']))
 
-            subsets = df.groupby(subset_num(df['start_time']))
+        # temp
+        df['duration'] = df['duration'].mask(
+                            subset_num(df['start_time']) != subset_num(df['end_time']),
+                            subset_size * (subset_num(df['start_time']) + 1) - df['start_time'])
+        df['end_time'] = df['end_time'].mask(
+                            subset_num(df['start_time']) != subset_num(df['end_time']),
+                            df['start_time'] + df['duration'])
+        # TODO: spawn split-off days into other subset dataframes
 
-            # temp
-            df['duration'] = df['duration'].mask(
-                                subset_num(df['start_time']) != subset_num(df['end_time']),
-                                subset_size * (subset_num(df['start_time']) + 1) - df['start_time'])
-            df['end_time'] = df['end_time'].mask(
-                                subset_num(df['start_time']) != subset_num(df['end_time']),
-                                df['start_time'] + df['duration'])
-            # TODO: spawn split-off days into other subset dataframes
+        filtered_dfs = []
+        results = [None] * len(subsets)
 
-            filtered_dfs = []
-            results = [None] * len(subsets)
+        start = perf_counter()
+        q = int(args.resultant_sample_size * len(subsets))
+        subset_args = [[subset, q] for i, subset
+                in subsets]
 
-            start = perf_counter()
-<<<<<<< HEAD
-            with pool.Pool(processes=int(args.process_count)) as p:
-                if args.parallelize:
-                    tasks = []
-                    for i, subset in subsets:
-                        q = int(float(args.resultant_sample_size) * len(subset))
-                        p_args = [subset, q, results, int(i), times, None, None]
-                        tasks.append(p.apply_async(process_subset, p_args))
-                    p.close()
-                    p.join()
-                    for task in tasks:
-                        task.wait()
-                        filtered_dfs.append(task.get())
-                else:
-                    for i, subset in subsets:
-                        q = int(float(args.resultant_sample_size) * len(subset))
-                        filtered_dfs.append(process_subset(subset, q, results, i, times, progress_bar, progress_task) if args.visual else process_subset(subset, q, results, i, times, None, None))
-=======
-            q = int(args.resultant_sample_size * len(subsets))
-            subset_args = [[subset, q, i, progress_bar] for i, subset
-                    in subsets]
->>>>>>> 64b7ca29a4e19310ca7fe8c92c4bf3c132dd1ede
-
-            if args.parallelize:
-                with Pool(processes=args.process_count) as p:
-                    filtered_dfs = p.starmap(process_subset, subset_args)
-            else:
-                filtered_dfs = startmap(process_subset, subset_args)
-
-            print(f'Spent {start - perf_counter()}s sparcifying data"')
-            final_filtered_df = pd.concat(filtered_dfs)
+        if args.parallelize:
+            with Pool(processes=args.process_count) as p:
+                filtered_dfs = p.starmap(process_subset, subset_args)
         else:
-            q = int(args.resultant_sample_size * float(len(df)))
-            final_filtered_df = process_subset(df, q, None, None)
+            filtered_dfs = starmap(process_subset, subset_args)
 
-        if not os.path.exists(args.output_dir):
-            os.makedirs(args.output_dir)
+        print(f'Spent {start - perf_counter()}s sparcifying data"')
+        final_filtered_df = pd.concat(filtered_dfs)
+    else:
+        q = int(args.resultant_sample_size * float(len(df)))
+        final_filtered_df = process_subset(df, q, None, None)
 
-        final_filtered_df.to_csv(os.path.join(args.output_dir, 'visits.csv'), index=False)
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
-        # Ensure times is a dictionary with proper keys and values
-        times["total"] = end - start
-        times_df = pd.DataFrame(list(times.items()), columns=['Interval', 'Time'])
-        times_df.to_csv(os.path.join(args.output_dir, 'times.csv'), index=False)
+    final_filtered_df.to_csv(os.path.join(args.output_dir, 'visits.csv'), index=False)
 
-        print(f'complete: {os.path.join(args.output_dir, "visits.csv")}', flush=True)
+    # Ensure times is a dictionary with proper keys and values
+    times["total"] = end - start
+    times_df = pd.DataFrame(list(times.items()), columns=['Interval', 'Time'])
+    times_df.to_csv(os.path.join(args.output_dir, 'times.csv'), index=False)
+
+    print(f'complete: {os.path.join(args.output_dir, "visits.csv")}', flush=True)
 
 if __name__ == "__main__":
     main()
