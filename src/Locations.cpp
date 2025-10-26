ba0a77f99b8cc638cb0936732bd629c4d2ac2864
Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro.local>
2025-10-25 20:59:50 -0400

    Lint fixes again

--- src/Locations.cpp
+++ src/Locations.cpp
@@ -71,7 +71,7 @@ Locations::Locations(int seed, std::string scenarioPath) {
   }
 
   // Load application data if not running on the fly.
-  // If the scenario was on the fly, this will be done in ReceiveVisitSchedule, 
+  // If the scenario was on the fly, this will be done in ReceiveVisitSchedule,
   // invoked from the people chares.
   if (!scenario->isOnTheFly()) {
     loadLocationData(scenarioPath);
