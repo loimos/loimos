ef218355985f50d6239ba3d15f36e616c9d01c07
Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro.local>
2026-01-12 17:27:58 -0500

    Removing forward declarations and moving them into header

--- src/DiscreteEventSimulation.cpp
+++ src/DiscreteEventSimulation.cpp
@@ -20,42 +20,42 @@
 #include <algorithm>
 #include <unordered_map>
 
-// Forward declarations
-#if OUTPUT_FLAGS & OUTPUT_OVERLAPS
-Counter saveInteractions(const Location &loc,
-    const Event &departure, std::ofstream *out,
-    const std::vector<Event> &susceptibleArrivals,
-    const std::vector<Event> &infectiousArrivals);
-#endif  // OUTPUT_OVERLAPS
+// // Forward declarations
+// #if OUTPUT_FLAGS & OUTPUT_OVERLAPS
+// Counter saveInteractions(const Location &loc,
+//     const Event &departure, std::ofstream *out,
+//     const std::vector<Event> &susceptibleArrivals,
+//     const std::vector<Event> &infectiousArrivals);
+// #endif  // OUTPUT_OVERLAPS
 
-// Helper functions to handle when a person leaves a location
-// onDeparture branches to one of the two other functions
-inline void onDeparture(Location *loc, Scenario *scenario, const Event& departure,
-    const std::vector<Event> &susceptibleArrivals,
-    const std::vector<Event> &infectiousArrivals,
-    std::unordered_map<Id, std::vector<Interaction>> *interactions, int thisIndex);
-void onSusceptibleDeparture(Location *loc, Scenario *scenario,
-    const Event& susceptibleDeparture,
-    const std::vector<Event> &infectiousArrivals,
-    std::unordered_map<Id, std::vector<Interaction>> *interactions, int thisIndex);
-void onInfectiousDeparture(Location *loc, Scenario *scenario,
-    const Event& infectiousDeparture,
-    const std::vector<Event> &susceptibleArrivals,
-    std::unordered_map<Id, std::vector<Interaction>> *interactions);
+// // Helper functions to handle when a person leaves a location
+// // onDeparture branches to one of the two other functions
+// inline void onDeparture(Location *loc, Scenario *scenario, const Event& departure,
+//     const std::vector<Event> &susceptibleArrivals,
+//     const std::vector<Event> &infectiousArrivals,
+//     std::unordered_map<Id, std::vector<Interaction>> *interactions, int thisIndex);
+// void onSusceptibleDeparture(Location *loc, Scenario *scenario,
+//     const Event& susceptibleDeparture,
+//     const std::vector<Event> &infectiousArrivals,
+//     std::unordered_map<Id, std::vector<Interaction>> *interactions, int thisIndex);
+// void onInfectiousDeparture(Location *loc, Scenario *scenario,
+//     const Event& infectiousDeparture,
+//     const std::vector<Event> &susceptibleArrivals,
+//     std::unordered_map<Id, std::vector<Interaction>> *interactions);
 
-// Helper function which packages all the neccessary information about
-// an interaction between a susceptible person and an infectious person
-// and add it to the approriate list for the susceptible person
-inline void registerInteraction(Location *loc, Scenario *scenario,
-    const Event &susceptibleEvent, const Event &infectiousEvent,
-    Time startTime, Time endTime,
-    std::unordered_map<Id, std::vector<Interaction>> *interactions);
+// // Helper function which packages all the neccessary information about
+// // an interaction between a susceptible person and an infectious person
+// // and add it to the approriate list for the susceptible person
+// inline void registerInteraction(Location *loc, Scenario *scenario,
+//     const Event &susceptibleEvent, const Event &infectiousEvent,
+//     Time startTime, Time endTime,
+//     std::unordered_map<Id, std::vector<Interaction>> *interactions);
 
-// Simple helper function which send the list of interactions with the
-// specified person to the appropriate People chare
-inline void sendInteractions(Location *loc, Scenario *scenario,
-    Id personIdx, std::unordered_map<Id, std::vector<Interaction>> *interactions,
-    int thisIndex);
+// // Simple helper function which send the list of interactions with the
+// // specified person to the appropriate People chare
+// inline void sendInteractions(Location *loc, Scenario *scenario,
+//     Id personIdx, std::unordered_map<Id, std::vector<Interaction>> *interactions,
+//     int thisIndex);
 
 Counter processEvents(Location *loc, Scenario *scenario,
   std::ofstream *interactionsFile, int thisIndex) {
