260cbba1e1128fd6e4d6f71dd10a316eca98741d
Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro.local>
2026-01-12 18:11:51 -0500

    Added comments to header file

--- src/DiscreteEventSimulation.cpp
+++ src/DiscreteEventSimulation.cpp
@@ -104,6 +104,9 @@ Counter processEvents(Location *loc, Scenario *scenario,
 #endif  // DEBUG_VERBOSE
 }
 
+// Writes a line per overlap containing information about
+// the overlapping people. For debugging/instrumentation 
+// purposes. 
 #if OUTPUT_FLAGS & OUTPUT_OVERLAPS
 Counter saveInteractions(const Location &loc,
     const Event &departure, std::ofstream *out,
@@ -157,6 +160,8 @@ inline void onDeparture(Location *loc, Scenario *scenario, const Event& departur
   }
 }
 
+// Handles a susceptible person's departure, registering any interactions
+// with infectious people. 
 void onSusceptibleDeparture(Location *loc, Scenario *scenario,
     const Event& susceptibleDeparture, const std::vector<Event> &infectiousArrivals,
     std::unordered_map<Id, std::vector<Interaction>> *interactions, int thisIndex) {
@@ -174,6 +179,8 @@ void onSusceptibleDeparture(Location *loc, Scenario *scenario,
     thisIndex);
 }
 
+// Handles an infectious person's departure, registering any interactions 
+// with susceptible people. 
 void onInfectiousDeparture(Location *loc, Scenario *scenario,
     const Event& infectiousDeparture, const std::vector<Event> &susceptibleArrivals,
     std::unordered_map<Id, std::vector<Interaction>> *interactions) {
--- src/DiscreteEventSimulation.h
+++ src/DiscreteEventSimulation.h
@@ -15,9 +15,13 @@
 #include <vector>
 #include <unordered_map>
 
+// Processes all of the current events and returns indices of 
+// people who have been infected.
 Counter processEvents(Location *loc, Scenario *scenario,
     std::ofstream *interactionsFile, int thisIndex);
 
+// Debugging/Instrumentation - 
+// Writes visit overlaps to an output stream 
 #if OUTPUT_FLAGS & OUTPUT_OVERLAPS
 Counter saveInteractions(const Location &loc,
     const Event &departure, std::ofstream *out,
@@ -25,6 +29,8 @@ Counter saveInteractions(const Location &loc,
     const std::vector<Event> &infectiousArrivals);
 #endif  // OUTPUT_OVERLAPS
 
+// Helper functions that handle when a person leaves a location. 
+// onDeparture branches to one of the other two. 
 inline void onDeparture(Location *loc, Scenario *scenario, const Event& departure,
     const std::vector<Event> &susceptibleArrivals,
     const std::vector<Event> &infectiousArrivals,
@@ -38,11 +44,14 @@ void onInfectiousDeparture(Location *loc, Scenario *scenario,
     const std::vector<Event> &susceptibleArrivals,
     std::unordered_map<Id, std::vector<Interaction>> *interactions);
 
+// Registers information about an interaction between a susceptible 
+// and infectious person
 inline void registerInteraction(Location *loc, Scenario *scenario,
     const Event &susceptibleEvent, const Event &infectiousEvent,
     Time startTime, Time endTime,
     std::unordered_map<Id, std::vector<Interaction>> *interactions);
 
+// Sends interactions to appropriate People chare
 inline void sendInteractions(Location *loc, Scenario *scenario,
     Id personIdx, std::unordered_map<Id, std::vector<Interaction>> *interactions,
     int thisIndex);
