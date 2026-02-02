2f20f55893a8de51423031ac779aa507be8bb3c0
Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro.local>
2026-02-02 17:11:08 -0500

    Add basic reusable setup code for all tests + add a basic test

--- src/tests/QueueVisitsTest.cpp
+++ src/tests/QueueVisitsTest.cpp
@@ -2,15 +2,17 @@
 
 #include "Locations.h"
 #include "Location.h"
-#include "VisitMessage.h"
-#include "Event.h"
+#include "Message.h"
 #include "Scenario.h"
 #include "DiseaseModel.h"
+#include "Types.h"
+#include "Defs.h"
 
 class FakeDiseaseModel : public DiseaseModel {
 public:
-    // what is diseasestate - whats the necessary import?
-  bool isInfectious(DiseaseState personState) const override {
+  // false for simplicity -- only needed if we want to test SC
+  bool isInfectious(DiseaseState) const override {
+    return false;
   }
 };
 
@@ -23,3 +25,59 @@ struct FakeScenario : public Scenario {
   FakeDiseaseModel fakeDiseaseModel;
 };
 
+static Locations makeBasicLocationsSetup() {
+  FakeScenario* scenario = new FakeScenario();
+
+  Locations locs;
+  locs.scenario = scenario;
+  locs.day = 0;
+
+  // 1 location
+  Location location;
+  locs.locations.push_back(location);
+
+  // visitsByDay must be sized
+  locs.locations[0].visitsByDay.resize(1);
+
+  // 1 active visit
+  VisitMessage visit;
+  visit.personIdx = 0;
+  visit.visitStart = 10;
+  visit.visitEnd = 20;
+  visit.deactivatedBy = 0; // anything other than -1 is 'active'
+
+  locs.locations[0].visitsByDay[0].push_back(visit);
+
+  PersonState ps;
+  ps.state = 1;
+  ps.transmissionModifier = 1.0;
+
+  locs.visitorStates.emplace(0, ps);
+
+  return locs;
+}
+
+
+TEST(LocationsTest, QueueVisitsImpl_CreatesArrivalAndDepartureEvents) {
+  Locations locs = makeBasicLocationsSetup();
+
+  locs.queueVisitsImpl();
+
+  const auto& events = locs.locations[0].events;
+
+  ASSERT_EQ(events.size(), 2);
+
+  // Arrival event
+  EXPECT_EQ(events[0].type, ARRIVAL);
+  EXPECT_EQ(events[0].personIdx, 0);
+  EXPECT_EQ(events[0].time, 10);
+
+  // Departure event
+  EXPECT_EQ(events[1].type, DEPARTURE);
+  EXPECT_EQ(events[1].personIdx, 0);
+  EXPECT_EQ(events[1].time, 20);
+
+  // Check pairings
+  EXPECT_EQ(events[0].pairedEvent, &events[1]);
+  EXPECT_EQ(events[1].pairedEvent, &events[0]);
+}
