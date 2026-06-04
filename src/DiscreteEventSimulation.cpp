9a93636a08bcb95b92e59da50fc3dbc709108413
Yash Sakharkar <64757237+ysakharkar@users.noreply.github.com>
2026-06-04 16:36:08 -0400

    Interaction message rework - batch sends and decoupling from DES (#149)
    * Change definition of ReceiveInteractions in ci file
    
    * Change implementation of ReceiveInteractions to handle vector
    
    * Remove interaction sending from DES and enable batch sending them from Locations
    
    * Remove Extern.h dependency in DES
    
    * Fix compile errors by matching Main.cpp calls to new ReceiveInteractions defn. Also remove defunct hypercomm ifdefs
    
    * Fix Main.cpp hypercomm defunct code
    
    * Fix definition of processEvents in DES
    
    * Rename a helper and fix the call to it
    
    * Fix definition of helper in header file
    
    * Change to cpp17 to remove warnings about structured bindings
    
    * Linter fixes
    
    * Documentation + small compilation error fix
    
    * Some more linter fixes
    
    * Remove swp file
    
    ---------
    
    Co-authored-by: Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro.local>
    Co-authored-by: Yash Sakharkar <ysakharkar@Yashs-MacBook-Pro-2.local>

--- src/DiscreteEventSimulation.cpp
+++ src/DiscreteEventSimulation.cpp
@@ -14,7 +14,6 @@
 #include "Scenario.h"
 #include "Interaction.h"
 #include "contact_model/ContactModel.h"
-#include "Extern.h"
 
 #include <vector>
 #include <algorithm>
@@ -23,11 +22,11 @@
 // Runs through all of the current events and return the indices of
 // any people who have been infected
 Counter processEvents(Location *loc, Scenario *scenario,
-  std::ofstream *interactionsFile, int thisIndex) {
+  std::ofstream *interactionsFile, int thisIndex,
+  std::unordered_map<Id, std::vector<Interaction>> *interactions) {
   std::vector<Event> *arrivals;
   std::vector<Event> infectiousArrivals;
   std::vector<Event> susceptibleArrivals;
-  std::unordered_map<Id, std::vector<Interaction>> interactions;
 #if ENABLE_DEBUG >= DEBUG_VERBOSE
   Counter numInteractions = 0;
   Counter numPresent = 0;
@@ -80,7 +79,7 @@ Counter processEvents(Location *loc, Scenario *scenario,
 #endif
 
       onDeparture(loc, scenario, event, susceptibleArrivals, infectiousArrivals,
-        &interactions, thisIndex);
+        interactions, thisIndex);
     }
   }
   loc->reset();
@@ -174,9 +173,6 @@ void onSusceptibleDeparture(Location *loc, Scenario *scenario,
         susceptibleDeparture.partnerTime),
         susceptibleDeparture.scheduledTime, interactions);
   }
-
-  sendInteractions(loc, scenario, susceptibleDeparture.personIdx, interactions,
-    thisIndex);
 }
 
 // Handles an infectious person's departure, registering any interactions
@@ -197,7 +193,7 @@ void onInfectiousDeparture(Location *loc, Scenario *scenario,
 
 // Helper function which packages all the necessary information about
 // an interaction between a susceptible person and an infectious person
-// and add it to the approriate list for the susceptible person
+// and adds it to the appropriate list for the susceptible person
 inline void registerInteraction(Location *loc, Scenario *scenario,
     const Event &susceptibleEvent, const Event &infectiousEvent, Time startTime,
     Time endTime, std::unordered_map<Id, std::vector<Interaction>> *interactions) {
@@ -209,44 +205,9 @@ inline void registerInteraction(Location *loc, Scenario *scenario,
     susceptibleEvent.personState, infectiousEvent.personState, startTime, endTime,
     susceptibleEvent.transmissionModifier, infectiousEvent.transmissionModifier);
 
-  // Note that this will create a new vector if this is the first potential
-  // infection for the susceptible person in question
+  // Note that this will create a new vector if this is the first potential infection
+  // for the susceptible person in question
   Interaction inter { propensity, infectiousEvent.personIdx,
     infectiousEvent.personState, startTime, endTime };
   (*interactions)[susceptibleEvent.personIdx].emplace_back(inter);
-}
-
-// Simple helper function which send the list of interactions with the
-// specified person to the appropriate People chare
-inline void sendInteractions(Location *loc, Scenario *scenario,
-    Id personIdx, std::unordered_map<Id, std::vector<Interaction>> *interactions,
-    int thisIndex) {
-  Partitioner *partitioner = scenario->partitioner;
-  PartitionId personPartition = partitioner->getPersonPartitionIndex(personIdx);
-#ifdef ENABLE_DEBUG
-  if (outOfBounds(0, partitioner->getNumPersonPartitions(), personPartition)) {
-    CkAbort("Error on chare %d: sending exposures at "
-      ID_PRINT_TYPE" to person " ID_PRINT_TYPE " on chare "
-      PARTITION_ID_PRINT_TYPE" outside of valid range [0, "
-      PARTITION_ID_PRINT_TYPE")\n", thisIndex, loc->getUniqueId(),
-      personIdx, personPartition, partitioner->getNumPersonPartitions());
-  }
-#endif
-
-  InteractionMessage interMsg(loc->getUniqueId(), personIdx,
-      (*interactions)[personIdx]);
-#ifdef USE_HYPERCOMM
-  Aggregator *agg = aggregatorProxy.ckLocalBranch();
-  if (agg->interact_aggregator) {
-    agg->interact_aggregator->send(peopleArray[personPartition], interMsg);
-    continue;
-  }
-#endif  // USE_HYPERCOMM
-
-  peopleArray[personPartition].ReceiveInteractions(interMsg);
-
-  // Free up space where we were storing interactions data. This also prevents
-  // interactions from being sent multiple times if this person has multiple
-  // visits to this location
-  interactions->erase(personIdx);
 }
